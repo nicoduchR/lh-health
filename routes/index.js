6f368518f3760d2189fde78dc8b0de0dd2a72010
Duchemann <nicolas.duchemann@redlab.io>
2020-06-27 15:03:56 +0200

    Starting implement BT

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: '' });
 });
 
+
+
+const bluetooth = require('node-bluetooth');
+ 
+// create bluetooth device instance
+const device = new bluetooth.DeviceINQ();
+
+
+
+device
+.on('finished',  console.log.bind(console, 'finished'))
+.on('found', function found(address, name){
+  console.log('Found: ' + address + ' with name ' + name);
+}).scan();
+
+
 module.exports = router;
