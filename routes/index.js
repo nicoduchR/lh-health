caf5088892d73e2698117979a5132864a96f4d20
Duchemann <nicolas.duchemann@redlab.io>
2020-06-30 23:54:43 +0200

    wololo

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,44 @@
 var express = require('express');
 var router = express.Router();
-
+const nodemailer = require("nodemailer");
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: '' });
 });
 
+router.get('/wololo', async function(req, res, next) {
+  let body = req.body;
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: 'smtp.office365.com', // Office 365 server
+    port: 587,     // secure SMTP
+    secure: false, // false for TLS - as a boolean not string - but the default is false so just remove this completely
+    auth: {
+        user: 'nicolas.duchemann@redlab.io',
+        pass: "#Canoute1"
+    },
+    tls: {
+        ciphers: 'SSLv3'
+    }
+  });
 
+  
+  // send mail with defined transport object
+  let info = await transporter.sendMail({
+    from: '"LH-Health" <nicolas.duchemann@redlab.io>', // sender address
+    to: "nicolas.duchemann@redlab.io", // list of receivers
+    subject: "Test", // Subject line
+    text: "", // plain text body
+    html: `
 
-const bluetooth = require('node-bluetooth');
- 
-// create bluetooth device instance
-const device = new bluetooth.DeviceINQ();
+    'Message reçu !! Je continue ça demain mdr'
+    
+    ` // html body
+    });
 
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).send('OK');
 
-
-device
-.on('finished',  console.log.bind(console, 'finished'))
-.on('found', function found(address, name){
-  console.log('Found: ' + address + ' with name ' + name);
-}).scan();
-
+});
 
 module.exports = router;
