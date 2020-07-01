b3b35573655f7d3fcc5eede5166d3126cb4b9ee8
Duchemann <nicolas.duchemann@redlab.io>
2020-07-01 20:22:16 +0200

    del email

--- routes/index.js
+++ routes/index.js
@@ -8,36 +8,9 @@ router.get('/', function(req, res, next) {
 
 router.get('/wololo', async function(req, res, next) {
   let body = req.body;
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: 'smtp.office365.com', // Office 365 server
-    port: 587,     // secure SMTP
-    secure: false, // false for TLS - as a boolean not string - but the default is false so just remove this completely
-    auth: {
-        user: 'nicolas.duchemann@redlab.io',
-        pass: "#Canoute1"
-    },
-    tls: {
-        ciphers: 'SSLv3'
-    }
-  });
-
-  
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: '"LH-Health" <nicolas.duchemann@redlab.io>', // sender address
-    to: "nicolas.duchemann@redlab.io", // list of receivers
-    subject: "Test", // Subject line
-    text: "", // plain text body
-    html: `
-
-    'Message reçu !! Je continue ça demain mdr'
-    
-    ` // html body
-    });
-
+    console.log(body);
     res.setHeader('Content-Type', 'application/json');
-    res.status(200).send('OK');
+    res.status(200).send(body);
 
 });
 
